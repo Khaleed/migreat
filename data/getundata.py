80d2a7654f207045e44c512405111bc9bf291bfc
Khalid Omar Ali <khalid.ali@consultant.com>
2015-12-09 17:20:13 -0500

    got all the immigration data for all the years

--- data/getundata.py
+++ data/getundata.py
@@ -1,26 +1,49 @@
 from __future__ import division
 import pandas as pd
 import numpy as np
+import csv
 
-file_name = 'OriginAndDestination_2013T110/1990.csv'
-dest = 'United States of America'
+def transform_data(in_file, out_file):
 
-with open(file_name, 'r') as f:
-	# read csv
-	data = pd.read_csv(f)
+	dest = 'United States of America'
 
-row = data.loc[data['Destination'] == dest]
+	with open(in_file, 'r') as f:
+		# read csv
+		data = pd.read_csv(f)
 
-origins = data.loc[data['Country code'] < 900]
+	row = data.loc[data['Destination'] == dest]
 
-countries = origins['Destination']
+	origins = data.loc[data['Country code'] < 900]
 
-# list comprehension
-vals = [(i, row[i].values[0]) for i in countries]
+	countries = origins['Destination']
 
-# filter
-filtered_vals = filter(lambda x: not np.isnan(x[1]), vals)
+	# list comprehension
+	vals = [(i, row[i].values[0]) for i in countries]
 
-# get the iso codes for origins 
+	# filter
+	filtered_vals = filter(lambda x: not np.isnan(x[1]), vals)
 
+	# sort the values in descending order
+	sorted_vals = sorted(filtered_vals, reverse=True, key=lambda x: x[1])
 
+	# get the top 20
+	top_20 = sorted_vals[:20]
+
+	# go through each pair of destinationa and code and set it
+	country_to_code = { d: c for d, c in zip(data['Destination'], data['Country code'])}
+
+	# we need list of top 20 origins, their iso codes and values
+	result = [(country_to_code[n], n, int(v)) for n, v in top_20]
+	# print to csv
+	with open(out_file, 'w') as f:
+		# write to csv
+		writer = csv.writer(f)
+		writer.writerow(['iso', 'name', 'value'])
+		for row in result:
+			writer.writerow(row)
+
+years = [1990, 2000, 2010, 2013]
+for y in years :
+	in_file = 'originAndDestination/%d.csv' % y
+	out_file = 'output/us%d.csv' % y
+	transform_data(in_file, out_file)
