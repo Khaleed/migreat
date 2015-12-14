52d21c1df5c1ca2081a999e5716b34a26239afd0
Khalid Omar Ali <khalid.ali@consultant.com>
2015-12-13 20:47:08 -0500

    can now animate migrant flow to the US for the top 20 country of origin

--- app/js/components/worldmap/animation.js
+++ app/js/components/worldmap/animation.js
@@ -1,29 +1,21 @@
 // grab modules
-import {
-	countriesToCentroids
-}
-from './map';
+import { countriesToCentroids } from './map';
 let d3 = require("d3");
 let _ = require('lodash');
 
 // grab variables for life cycles
 let screen = document.getElementById('screen');
 const totalArrowTime = 60;
-const migrantsPerArrow = 4000;
-let migrantsPerCountry = 10000;
-
-// // let distanceBetweenDestAndSrc = Math.sqrt(Math.pow(latSource - latDest, 2) + Math.pow(longSource - longDest, 2));
-// let getDistance = (latSource, longSource, latDest, longDest) => {
-// 	return Math.sqrt(Math.pow(latSource - latDest, 2) + Math.pow(longSource - longDest, 2));
-// };
+const migrantsPerArrow = 40000;
 
 let destinations = null;
 // load data
-let immigrationData = d3.csv("us2013.csv", (error, data) => {
+let immigrationData = d3.csv("us2013.csv", (error, data) => { 
 	if (error) {
 		console.error(error);
 	} else {
 		destinations = {
+			// US
 			840: data
 		};
 		// see how ratio changes from 0 to 1
@@ -31,14 +23,8 @@ let immigrationData = d3.csv("us2013.csv", (error, data) => {
 	}
 });
 
-// let currentArrowsBeingAnimated = {
-//  	iso: [0.1, 0.2]
-// };
-
 // let getArrowsFromRatio = (migrantsPerArrow, destinationISO, ratio) => {
-// 	let migrantsData = destinations[destinationISO];
 // 	const b = getCentroid(destinationISO);
-// 	Object.keys(migrantsData).map((countryISO) => {
 // 		let totalImmigrantsFromACountry = migrantsData[countryISO];
 // 		let a = getCentroid(countryISO);
 // 		let distance = getDistance(a[0], a[1], b[0], b[1]);
@@ -55,35 +41,41 @@ let immigrationData = d3.csv("us2013.csv", (error, data) => {
 
 // 		// we need to return a list of arrows with fractionsAlongPath for each
 // 		getDistance();
-// 	});
 // };
 
 // generate arrows and pass that to a draw function
-let render = (fractionThroughTime) => {
-	//	console.log(fractionThroughTime);
-	// we need information about US and origins and fractionThroughTime
-	let fractionsAlongPath = [];
-	let points = migrantsPerCountry / migrantsPerArrow;
-	for (let i = 0; i < points; i += 1) {
-		let fraction = i / points + fractionThroughTime;
-		if (fraction < 1) {
-			fractionsAlongPath.push(fraction);
-		}
-	}
-	let arrows = {
-		//		192: [fractionThroughTime]
-		192: fractionsAlongPath
-			//		192: [0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.8, 1] // -> this data is mock/dummy -> instead of this I need to use
-	};
-	// let arrows = getArrowsFromRatio(migrantsPerArrow, 840, fractionThroughTime)
-	destinations[840]
+let render = fractionThroughTime => {
+	// loop through countries here
+	let migrantsData = destinations[840];
+	let arrows = _.reduce(migrantsData, (arrows, originCountry) => {
+		// we need information about US and origins and fractionThroughTime
+		let migrantsPerCountry = originCountry.value;
+		let countryISO = originCountry.iso;
+		let points = migrantsPerCountry / migrantsPerArrow;
+		arrows[countryISO] = _.range(points).reduce((list, i) => {
+			let fraction = i / points + fractionThroughTime;
+			if (fraction < 1) {
+				list.push(fraction);
+			}
+			return list;
+		}, []);
+		return arrows; 
+	}, {});
 	drawArrows(arrows, 840, screen);
 };
+
+// let arrows = {
+// 		//		192: [fractionThroughTime]
+// 		192: fractionsAlongPath
+
+// 		// 192: [0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.8, 1] // -> this data is mock/dummy -> instead of this I need to use
+// 	};
+
 // takes ISO and returns it's centroid
 let getCentroid = iso => {
 	return countriesToCentroids[iso];
 };
-
+// unit vector 
 let bMinusA = (a, b) => {
 	return [b[0] - a[0], b[1] - a[1]];
 };
@@ -109,7 +101,7 @@ let drawArrows = (arrows, destination, screen) => {
 	ctx.fillStyle = "brown";
 	// recursive function instead of forEach
 	arrowCoordinates.forEach(c => {
-		ctx.fillRect(c[0], c[1], 10, 10);
+		ctx.fillRect(c[0], c[1], 1, 1);
 	});
 };
 
