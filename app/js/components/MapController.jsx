3d3b3bf4e83e1648f1debc50c6daa1928d8a2bca
Khalid <khalid@alum.recurse.com>
2017-10-31 23:38:55 +0000

    Fix indentation

--- app/js/components/MapController.jsx
+++ app/js/components/MapController.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var {zoomMap} = require('./worldmap/map'); 
+var {zoomMap} = require('./worldmap/map');
 
 export const MapController = React.createClass({
 
@@ -8,7 +8,7 @@ export const MapController = React.createClass({
 	},
 
 	onPause() {
-		
+
 	},
 
 	onForward() {
@@ -19,29 +19,29 @@ export const MapController = React.createClass({
 
 	},
 
-	render() {
-		let styles = {
-			wrapper: {
-				width: "90%",
-				margin: "0 auto",
-				marginLeft: "1em",
+    render() {
+        let styles = {
+            wrapper: {
+                width: "90%",
+                margin: "0 auto",
+                marginLeft: "1em",
                 marginRight: "1em",
-    			marginTop: "1em",
-    			marginBottom: "4em"	
-			},
-			button: {
-				color: "green",
-				width: "25%",
-				height: "100px"
-			}
-		};
-		return (
-			<div style={styles.wrapper}>
-				<input style={styles.button} value="Play" type="button" onClick={this.onPlay} ></input>
-				<input style={styles.button} value="Pause" type="button" onClick={this.onPause} ></input>
-				<input style={styles.button} value="Forward" type="button" onClick={this.onForward} ></input>
-				<input style={styles.button} value="Rewind" type="button" onClick={this.onRewind} ></input>
-			</div>);
-		;
-	}
+                marginTop: "1em",
+                marginBottom: "4em"
+            },
+            button: {
+                color: "green",
+                width: "25%",
+                height: "100px"
+            }
+        };
+        return (
+            <div style={styles.wrapper}>
+                <input style={styles.button} value="Play" type="button" onClick={this.onPlay} ></input>
+                <input style={styles.button} value="Pause" type="button" onClick={this.onPause} ></input>
+                <input style={styles.button} value="Forward" type="button" onClick={this.onForward} ></input>
+				        <input style={styles.button} value="Rewind" type="button" onClick={this.onRewind} ></input>
+			      </div>);
+		    ;
+	  }
 });
